a628ecb56b84bcdab40579cbcd134b2d19ec19d1
nishantagrawal009 <nishantagrawal009@gmail.com>
2021-01-04 09:42:08 +0530

    adding migration as different image

--- package/orm/wire.go
+++ package/orm/wire.go
@@ -14,7 +14,7 @@ func InitialiseDb() Database {
 
 func InitialiseConfig() config {
 	wire.Build(wire.Value(config{
-		Url:      "192.168.0.3",
+		Url:      "192.168.0.2",
 		Port:     5432,
 		Dbname:   "fullstack_api",
 		Ssl:      "disable",
